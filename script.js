ae665f35bfadedc2d6bfdac197fa2e242a9b3816
nitinparmarn88-dotcom <nitinparmarn88@gmail.com>
2026-07-12 13:00:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let slides = document.querySelectorAll(".slide");
 
+let index = 0;
+
+setInterval(() => {
+
+slides[index].classList.remove("active");
+
+index++;
+
+if(index>=slides.length){
+
+index=0;
+
+}
+
+slides[index].classList.add("active");
+
+},3000);
