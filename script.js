09e9cefcaf0d6dad136aad858aab9c89c9060725
nitinparmarn88-dotcom <nitinparmarn88@gmail.com>
2026-07-12 13:31:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+setInterval(() => {
+
+slides[index].classList.remove("active");
+
+index++;
+
+if(index>=slides.length){
+
+index=0;
+
+}
+
+slides[index].classList.add("active");
+
+},3000);
+
 const form = document.querySelector("#booking form");
 
 form.addEventListener("submit", function(e) {
