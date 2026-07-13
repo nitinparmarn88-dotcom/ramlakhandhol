5d36a1fda3822f49544f677d9eb463b0b45c849c
nitinparmarn88-dotcom <nitinparmarn88@gmail.com>
2026-07-13 08:53:19 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import {
   getAuth,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  signOut
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
