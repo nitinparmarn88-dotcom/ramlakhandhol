e165e5d6e37c3ccef0db1452cd263ad054fd3fc6
nitinparmarn88-dotcom <nitinparmarn88@gmail.com>
2026-07-13 13:31:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -189,3 +189,22 @@ async function loadVideo() {
 }
 
 loadVideo();
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach((entry)=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document.querySelectorAll(".fade-up").forEach((el)=>{
+
+observer.observe(el);
+
+});
